e6be8f8172498e31c5e9e26fc62549773b2a47ed
Gabriel Sgarlato <ga.sgarlato.10@gmail.com>
2026-04-16 09:53:48 -0400

    updated

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 import { auth, db } from "./firebase-config.js";
+
+const popup = document.getElementById("myPopup");
+document.getElementById("openBtn").onclick = () => popup.style.display = "block";
+document.getElementById("closeBtn").onclick = () => popup.style.display = "none";
